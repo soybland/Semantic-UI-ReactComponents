2c7bbd1f172b4a9d703bc26864718b0dd5bf6377
Seia-Soto <seia@outlook.kr>
2020-01-15 07:56:39 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,27 @@ module.exports = {
     node: true
   },
   extends: [
-    'standard'
+    'standard',
+    'standard-react',
+    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018
   },
   rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: 'first'
+      }
+    ],
+    'no-extend-native': 0 // NOTE: DO NOT OVERRIDE ORIGINAL FUNCTIONS.
   }
 }
